4e3062a69493afc9ecec8d4c9bc872aac2bf82eb
defendimattia <defendi.mattia92@gmail.com>
2025-07-03 15:47:41 +0200

    Add body to aggiungiAppuntamento, rimuoviAppuntamento, VisualizzaAppuntamenti, cercaPerData

--- src/org/project/agenzia/Agenda.java
+++ src/org/project/agenzia/Agenda.java
@@ -7,19 +7,36 @@ public class Agenda {
     private ArrayList<Appuntamento> listaAppuntamenti = new ArrayList<>();
 
     public void aggiungiAppuntamento(Appuntamento newAppuntamento) {
-
+        listaAppuntamenti.add(newAppuntamento);
     }
 
-    public void RimuoviAppuntamento() {
-
+    public void rimuoviAppuntamento(Appuntamento appuntamentoToDelete) {
+        listaAppuntamenti.remove(appuntamentoToDelete);
     }
 
     public void visualizzaAppuntamenti() {
-
+        for (Appuntamento appuntamento : listaAppuntamenti) {
+            System.out
+                    .println(appuntamento.getTitolo() + " " + appuntamento.getData() + " " + appuntamento.getOrario());
+        }
     }
 
-    public void cercaPerData() {
+    public void cercaPerData(String dataToSearch) {
+        ArrayList<Appuntamento> appuntamentiTrovati = new ArrayList<>();
 
+        for (Appuntamento appuntamento : listaAppuntamenti) {
+            if (appuntamento.getData().equals(dataToSearch)) {
+                appuntamentiTrovati.add(appuntamento);
+            }
+        }
+
+        if (appuntamentiTrovati.size() == 0) {
+            System.out.println("Nessun appuntamento trovato");
+        } else {
+            for (Appuntamento appuntamento : appuntamentiTrovati) {
+                appuntamento.stampaDettagli();
+            }
+        }
     }
 
     static int contaAppuntamenti() {

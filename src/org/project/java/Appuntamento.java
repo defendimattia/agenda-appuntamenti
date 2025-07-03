481c6ca7613ddab1707b91f110ee4bd529c73114
defendimattia <defendi.mattia92@gmail.com>
2025-07-03 10:59:26 +0200

    Add Appuntamento constructors

--- src/org/project/java/Appuntamento.java
+++ src/org/project/java/Appuntamento.java
@@ -7,4 +7,16 @@ public class Appuntamento {
     private String data; // formato gg/mm/aaaa
     private String orario; // formato hh:mm
 
+    public Appuntamento(String titolo, String descrizione, String data, String orario) {
+        this.titolo = titolo;
+        this.descrizione = descrizione;
+        this.data = data;
+        this.orario = orario;
+    }
+
+    public Appuntamento(String titolo, String data, String orario) {
+        this.titolo = titolo;
+        this.data = data;
+        this.orario = orario;
+    }
 }

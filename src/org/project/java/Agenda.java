ff34ab5a3caf616b074d277473b5f9f6c0bda57e
defendimattia <defendi.mattia92@gmail.com>
2025-07-03 10:34:17 +0200

    Add attributes to Appuntamento and Agenda classes

--- src/org/project/java/Agenda.java
+++ src/org/project/java/Agenda.java
@@ -1,5 +1,9 @@
 package org.project.java;
 
+import java.util.ArrayList;
+
 public class Agenda {
+
+    private ArrayList<Appuntamento> listaAppuntamenti = new ArrayList<>();
 
 }
--- src/org/project/java/Appuntamento.java
+++ src/org/project/java/Appuntamento.java
@@ -2,4 +2,9 @@ package org.project.java;
 
 public class Appuntamento {
 
+    private String titolo;
+    private String descrizione;
+    private String data; // formato gg/mm/aaaa
+    private String orario; // formato hh:mm
+
 }

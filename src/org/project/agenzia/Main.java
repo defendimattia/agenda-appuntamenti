e6926befc3c9735c51cc44e38f422f35892e2f8f
defendimattia <defendi.mattia92@gmail.com>
2025-07-04 10:47:47 +0200

    Add try and catch for userInput to prevent false values

--- src/org/project/agenzia/Main.java
+++ src/org/project/agenzia/Main.java
@@ -1,5 +1,6 @@
 package org.project.agenzia;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -17,9 +18,48 @@ public class Main {
             System.out.println("5. Mostra numero appuntamenti");
             System.out.println("6. Esci");
 
-            System.out.print("Scegli un'opzione: ");
-            int userInput = scanner.nextInt();
-        }
+            System.out.print("Scegli un numero: ");
+            try {
+                int userInput = scanner.nextInt();
 
+                switch (userInput) {
+                    case 1:
+
+                        break;
+
+                    case 2:
+
+                        break;
+
+                    case 3:
+
+                        break;
+
+                    case 4:
+
+                        break;
+
+                    case 5:
+                        System.out.println(Agenda.contaAppuntamenti());
+                        break;
+
+                    case 6:
+                        isRunning = false;
+                        break;
+
+                    default:
+                        System.out.println("Numero non valido. Riprova!");
+                        System.out.println("------------------------------");
+                }
+
+            } catch (InputMismatchException e) {
+                System.out.println("Inserisci un numero!");
+                System.out.println("------------------------------");
+                scanner.next();
+                continue;
+            }
+
+        }
+        scanner.close();
     }
 }
